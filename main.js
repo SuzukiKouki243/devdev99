afda51ab2008014036c347fbd9ef2d607ee4e06d
rascal999-sudo <75905681+rascal999-sudo@users.noreply.github.com>
2021-01-24 02:13:13 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-var http = require('http'); //httpモジュール呼び出し
-var server = http.createServer(function (request, response) {
-    // リクエストを受けると以下のレスポンスを送信する
-    response.writeHead(200, {'Content-Type': 'text/plain'}); //レスポンスヘッダーに書き込み
-    response.write('Hello World\n'); // レスポンスボディに「Hello World」を書き込み
-    response.end(); // レスポンス送信を完了する
+var express = require('express');
+
+var app = express.createServer();
+
+app.get('/', function(req, res) {
+    res.send('HerokuでNode.jsとExpressを使ってHello world!');
 });
-server.listen(80);  //8080番ポートで待ち受け
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log("Listening on " + port);
+});
